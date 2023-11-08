4717c6d324b2d44d11e2af14aa5594be1ad358a9
Alejandra <alegonzalez236@gmail.com>
2023-11-07 21:37:23 -0300

    preparando para deploy

--- app.js
+++ app.js
@@ -2,12 +2,12 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 
 //path?
 app.use('/static', express.static(__dirname + '/public'));
 
-app.listen(PORT, () => console.log("El servidor está escuchando en el puerto " + PORT));
+app.listen(PORT, () => console.log(`El servidor está escuchando en el puerto ${PORT}`));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/views/home.html');
